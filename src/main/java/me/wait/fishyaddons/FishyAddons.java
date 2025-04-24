56e8b208ae2fce2e1b3af3f0536cdf6a6b68fa13
valkeea <valkeeaa@gmail.com>
2025-04-24 03:09:52 +0300

    Add: Item protection

--- src/main/java/me/wait/fishyaddons/FishyAddons.java
+++ src/main/java/me/wait/fishyaddons/FishyAddons.java
@@ -2,12 +2,16 @@ package me.wait.fishyaddons;
 
 import me.wait.fishyaddons.config.ConfigHandler;
 import me.wait.fishyaddons.config.ParticleColorConfig;
+import me.wait.fishyaddons.config.UUIDConfigHandler;
+import me.wait.fishyaddons.fishyprotection.BlacklistConfigHandler;
 import me.wait.fishyaddons.handlers.KeybindHandler;
+import me.wait.fishyaddons.handlers.SellProtectionHandler;
 import me.wait.fishyaddons.handlers.AliasHandler;
 import me.wait.fishyaddons.handlers.FishyLavaHandler;
 import me.wait.fishyaddons.command.KeybindCommand;
 import me.wait.fishyaddons.command.CommandListCommand;
 import me.wait.fishyaddons.command.FishyAddonsCommand;
+import me.wait.fishyaddons.command.ProtectCommand;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.common.MinecraftForge;
@@ -25,14 +29,15 @@ public class FishyAddons {
     public static final String NAME = "FishyAddons";
     public static final String VERSION = "@VERSION@";
 
-    public static final KeyBinding openGUI = new KeyBinding("Open Keybind List", Keyboard.KEY_NONE, "FishyAddons");
+    public static final KeyBinding openGUI = new KeyBinding("Open Keybind List", Keyboard.KEY_NONE, NAME);
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
 
         ConfigHandler.initConfigPath(event.getModConfigurationDirectory());
         ConfigHandler.loadConfig();
-
+        BlacklistConfigHandler.loadUserBlacklist();
+        UUIDConfigHandler.loadConfig(); 
         ParticleColorConfig.invalidateCache();
     }
 
@@ -45,13 +50,14 @@ public class FishyAddons {
 
         FishyLavaHandler.updateRegistration();
         KeybindHandler.updateRegistration();
-
+        SellProtectionHandler.updateRegistration();
         KeybindHandler.refreshKeybindCache();
         AliasHandler.refreshCommandCache();
 
         ClientCommandHandler.instance.registerCommand(new KeybindCommand());
         ClientCommandHandler.instance.registerCommand(new FishyAddonsCommand());
         ClientCommandHandler.instance.registerCommand(new CommandListCommand());
+        ClientCommandHandler.instance.registerCommand(new ProtectCommand());
     }
 
     @Mod.EventHandler

7e406aa8e24b4c6798f00713115785cf4df622c9
valkeea <valkeeaa@gmail.com>
2025-04-24 03:25:31 +0300

    Add: Item protection

--- src/main/java/me/wait/fishyaddons/gui/FishyAddonsGUI.java
+++ src/main/java/me/wait/fishyaddons/gui/FishyAddonsGUI.java
@@ -22,22 +22,26 @@ public class FishyAddonsGUI extends GuiScreen {
         int centerX = width / 2;
         int centerY = height / 2;
 
-        toggleFishyLavaButton = new GuiButton(0, centerX - 100, centerY - 60, 200, 20, getToggleButtonText());
+        toggleFishyLavaButton = new GuiButton(0, centerX - 100, centerY - 80, 200, 20, getToggleButtonText());
         toggleFishyLavaButton.packedFGColour = ConfigHandler.isFishyLavaEnabled() ? 0xFFCCFFCC : 0xFFFF8080;
         buttonList.add(toggleFishyLavaButton);
 
-        openKeybindListButton = new GuiButton(1, centerX - 100, centerY - 30, 200, 20, "Keybind List");
+        openKeybindListButton = new GuiButton(1, centerX - 100, centerY - 50, 200, 20, "Keybind List");
         openKeybindListButton.packedFGColour = 0xFFE2CAE9;
         buttonList.add(openKeybindListButton);
 
-        openCommandAliasesButton = new GuiButton(4, centerX - 100, centerY, 200, 20, "Command Aliases");
+        openCommandAliasesButton = new GuiButton(4, centerX - 100, centerY - 20, 200, 20, "Command Aliases");
         openCommandAliasesButton.packedFGColour = 0xFFE2CAE9;
         buttonList.add(openCommandAliasesButton);
 
-        particleColorSlider = new CustomGuiSlider(3, centerX - 100, centerY + 30, 200, 20, "Custom Particle Color", 0, 4, ConfigHandler.getCustomParticleColorIndex());
+        GuiButton faProtectButton = new GuiButton(5, centerX - 100, centerY + 10, 200, 20, "Item Protection");
+        faProtectButton.packedFGColour = 0xFFE2CAE9;
+        buttonList.add(faProtectButton);
+
+        particleColorSlider = new CustomGuiSlider(3, centerX - 100, centerY + 40, 200, 20, "Custom Particle Color", 0, 4, ConfigHandler.getCustomParticleColorIndex());
         buttonList.add(particleColorSlider);
 
-        closeButton = new GuiButton(2, centerX - 100, centerY + 60, 200, 20, "Close");
+        closeButton = new GuiButton(2, centerX - 100, centerY + 70, 200, 20, "Close");
         buttonList.add(closeButton);
     }
 
@@ -51,7 +55,9 @@ public class FishyAddonsGUI extends GuiScreen {
         } else if (button.id == 1) {
             mc.displayGuiScreen(new KeybindListGUI());
         } else if (button.id == 4) {
-            mc.displayGuiScreen(new CommandListGUI());    
+            mc.displayGuiScreen(new CommandListGUI());
+        } else if (button.id == 5) {
+            mc.displayGuiScreen(new SellProtectionGUI());
         } else if (button.id == 2) {
             mc.displayGuiScreen(null);
         } else if (button.id == 3) {
@@ -66,7 +72,7 @@ public class FishyAddonsGUI extends GuiScreen {
     @Override
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
         drawDefaultBackground();
-        drawCenteredString(fontRendererObj, "FishyAddons", width / 2, height / 2 - 90, 0xFF55FFFF);
+        drawCenteredString(fontRendererObj, "FishyAddons", width / 2, height / 2 - 110, 0xFF55FFFF); // Adjusted header position
         super.drawScreen(mouseX, mouseY, partialTicks);
     }
 

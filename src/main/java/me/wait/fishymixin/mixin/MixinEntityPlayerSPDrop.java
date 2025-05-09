f2247f9cae3b83af3de6bc713c6ff413dfa41772
valkeea <valkeeaa@gmail.com>
2025-05-09 09:27:53 +0300

    Fix drops in dungeons

--- src/main/java/me/wait/fishymixin/mixin/MixinEntityPlayerSPDrop.java
+++ src/main/java/me/wait/fishymixin/mixin/MixinEntityPlayerSPDrop.java
@@ -3,6 +3,7 @@ package me.wait.fishymixin.mixin;
 import me.wait.fishyaddons.handlers.ProtectedItemHandler;
 import me.wait.fishyaddons.util.FishyNotis;
 import me.wait.fishyaddons.util.PlaySound;
+import me.wait.fishyaddons.util.ZoneUtils;
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.item.ItemStack;
@@ -11,13 +12,19 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-//Simple drop protection
+
 @Mixin(EntityPlayerSP.class)
 public class MixinEntityPlayerSPDrop {
 
     @Inject(method = "func_71040_bB", at = @At("HEAD"), cancellable = true)
     public void onDropItem(boolean dropAll, CallbackInfoReturnable<EntityItem> cir) {
-        EntityPlayerSP player = (EntityPlayerSP)(Object)this;
+        // Skip mixin logic if in a dungeon
+        if (ZoneUtils.isInDungeon()) {
+            System.out.println("Skipping drop item logic in dungeon.");
+            return;
+        }
+
+        EntityPlayerSP player = (EntityPlayerSP) (Object) this;
         ItemStack stack = player.inventory.getCurrentItem();
 
         if (ProtectedItemHandler.isProtected(stack)) {

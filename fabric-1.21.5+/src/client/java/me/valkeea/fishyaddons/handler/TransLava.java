2454e3d23447955e79ab7cc926613a6cd03d1bbd
valkeea <valkeeaa@gmail.com>
2025-09-16 04:54:54 +0300

    ref: added custom handler to allow more colors

--- fabric-1.21.5+/src/client/java/me/valkeea/fishyaddons/handler/TransLava.java
+++ fabric-1.21.5+/src/client/java/me/valkeea/fishyaddons/handler/TransLava.java
@@ -2,18 +2,21 @@ package me.valkeea.fishyaddons.handler;
 
 import me.valkeea.fishyaddons.config.FishyConfig;
 import me.valkeea.fishyaddons.config.Key;
+import me.valkeea.fishyaddons.impl.LavaRenderHandler;
+import me.valkeea.fishyaddons.util.AreaUtils;
 import me.valkeea.fishyaddons.util.SkyblockCheck;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderHandler;
 import net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderHandlerRegistry;
-import net.fabricmc.fabric.api.client.render.fluid.v1.SimpleFluidRenderHandler;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.WorldRenderer;
 import net.minecraft.client.texture.SpriteAtlasTexture;
 import net.minecraft.fluid.Fluids;
+import net.minecraft.util.Identifier;
 
 public class TransLava {
+    private TransLava() {}
     private static boolean isEnabled = false;
     private static int color = 0;
     private static FluidRenderHandler originalLavaHandler = null;
@@ -28,7 +31,9 @@ public class TransLava {
         boolean wasEnabled = isEnabled;
         int prevColor = color;
 
-        isEnabled = SkyblockCheck.getInstance().rules() && FishyConfig.getState(Key.FISHY_TRANS_LAVA, false);
+        isEnabled = SkyblockCheck.getInstance().rules() && AreaUtils.isCrimson()
+                && FishyConfig.getState(Key.FISHY_TRANS_LAVA, false);
+
         color = FishyConfig.getInt(Key.FISHY_TRANS_LAVA_COLOR);
 
         if (wasEnabled != isEnabled || (isEnabled && prevColor != color)) {
@@ -38,13 +43,16 @@ public class TransLava {
 
     private static void reloadRenderHandler() {
         if (isEnabled) {
-            FluidRenderHandler handler = SimpleFluidRenderHandler.coloredWater(color);
-            var id = SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE;
-            SpriteAtlasTexture texture = MinecraftClient.getInstance().getBakedModelManager().getAtlas(id);
+            FluidRenderHandler handler = LavaRenderHandler.coloredFluid(color);
+            
+            var atlasId = Identifier.ofVanilla("textures/atlas/blocks.png");
+            SpriteAtlasTexture texture = MinecraftClient.getInstance().getBakedModelManager().getAtlas(atlasId);
             handler.reloadTextures(texture);
 
-            FluidRenderHandlerRegistry.INSTANCE.register(Fluids.LAVA, Fluids.FLOWING_LAVA, handler);
+            FluidRenderHandlerRegistry.INSTANCE.register(Fluids.LAVA, handler);
+            FluidRenderHandlerRegistry.INSTANCE.register(Fluids.FLOWING_LAVA, handler);
             BlockRenderLayerMap.INSTANCE.putFluids(RenderLayer.getTranslucent(), Fluids.LAVA, Fluids.FLOWING_LAVA);
+            
         } else {
             FluidRenderHandlerRegistry.INSTANCE.register(Fluids.LAVA, originalLavaHandler);
             FluidRenderHandlerRegistry.INSTANCE.register(Fluids.FLOWING_LAVA, originalFlowingLavaHandler);
@@ -55,5 +63,13 @@ public class TransLava {
         if (renderer != null) {
             renderer.reload();
         }
+    }
+
+    public static boolean isEnabled() {
+        return isEnabled;
+    }
+
+    public static int getColor() {
+        return color;
     }
 }
